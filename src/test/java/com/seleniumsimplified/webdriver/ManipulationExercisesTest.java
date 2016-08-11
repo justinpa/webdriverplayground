a8cccc267a18329c699be4891c201676c29d8956
Justin Packham <justin.packham@gmail.com>
2016-08-11 23:16:44 +0200

    Exercises 2 to 4

--- src/test/java/com/seleniumsimplified/webdriver/ManipulationExercisesTest.java
+++ src/test/java/com/seleniumsimplified/webdriver/ManipulationExercisesTest.java
@@ -75,12 +75,84 @@ public class ManipulationExercisesTest {
             WebElement commentResult = driver.findElement(By.cssSelector("li[id='_valuecomments']"));
             assertEquals("Assert submitted comments", "TestComment", commentResult.getText());
 
-
         }catch(NoSuchElementException e){
             System.out.println("Failed");
             fail("exerciseTwoTest failed");
         }
     }
+
+
+    @Test
+    public void exerciseThreeTest(){
+        /*
+         This test submits radio 2 only
+         */
+
+        System.out.println("running exerciseThreeTest");
+
+        try {
+            driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_html_form.html");
+
+            WebElement radioButton2 = driver.findElement(By.cssSelector("input[type='radio'][value='rd2']"));
+            radioButton2.click();
+
+            WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
+            submitButton.click();
+            new WebDriverWait(driver, 10).until(ExpectedConditions.titleIs("Processed Form Details"));
+            assertEquals("Assert submitted page title", "Processed Form Details", driver.getTitle());
+
+            WebElement radioResult = driver.findElement(By.cssSelector("li[id='_valueradioval']"));
+            assertEquals("Assert submitted radio", "rd2", radioResult.getText());
+
+        }catch(NoSuchElementException e){
+            System.out.println("Failed");
+            fail("exerciseThreeTest failed");
+        }
+    }
+
+    
+
+    @Test
+    public void exerciseFourTest(){
+        /*
+         This test submits checkbox 3 only
+         */
+
+        System.out.println("running exerciseFourTest");
+
+        try {
+            driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_html_form.html");
+
+            WebElement checkbox1 = driver.findElement(By.cssSelector("input[type='checkbox'][value='cb1']"));
+            WebElement checkbox2 = driver.findElement(By.cssSelector("input[type='checkbox'][value='cb2']"));
+            WebElement checkbox3 = driver.findElement(By.cssSelector("input[type='checkbox'][value='cb3']"));
+
+            if (checkbox1.isSelected()){
+                checkbox1.click();
+            }
+
+            if (checkbox2.isSelected()){
+                checkbox2.click();
+            }
+
+            if (!checkbox3.isSelected()){
+                checkbox3.click();
+            }
+
+            WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
+            submitButton.click();
+            new WebDriverWait(driver, 10).until(ExpectedConditions.titleIs("Processed Form Details"));
+            assertEquals("Assert submitted page title", "Processed Form Details", driver.getTitle());
+
+            WebElement checkboxResult = driver.findElement(By.id("_valuecheckboxes0"));
+            assertEquals("Assert submitted checkbox", "cb3", checkboxResult.getText());
+
+        }catch(NoSuchElementException e){
+            System.out.println("Failed");
+            fail("exerciseFourTest failed");
+        }
+    }
+
 
 
 
